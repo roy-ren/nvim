5e1d0251bbe07836dc5585a4c139416e1affb9ea
roy <royite.ren@gmail.com>
2023-05-26 23:18:18 +0800

    feat: add theme base16

--- lua/roy/plugin/init.lua
+++ lua/roy/plugin/init.lua
@@ -8,17 +8,11 @@ local M = {
         lazy = false,
     },
     {
-        "folke/tokyonight.nvim",
-        lazy = false,    -- make sure we load this during startup if it is your main colorscheme
-        priority = 1000, -- make sure to load this before all the other start plugins
-        config = function()
-            -- load the colorscheme here
-            vim.cmd([[colorscheme tokyonight]])
-        end,
-    },
-    {
         'nvim-lualine/lualine.nvim',
-        dependencies = { 'nvim-tree/nvim-web-devicons', opt = true }
+        dependencies = {
+            { 'nvim-tree/nvim-web-devicons', opt = true },
+            { 'RRethy/nvim-base16' },
+        }
     },
     {
         'VonHeikemen/lsp-zero.nvim',
