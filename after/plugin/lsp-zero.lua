404910b32c0b41c7b3fc43554d615a21f51595c5
roy <royite.ren@gmail.com>
2023-05-26 22:47:33 +0800

    feat: Update autocompletion

--- after/plugin/lsp-zero.lua
+++ after/plugin/lsp-zero.lua
@@ -1,10 +1,62 @@
 local lsp = require('lsp-zero').preset({})
 
+-- setup after buffer on attach
 lsp.on_attach(function(client, bufnr)
-  lsp.default_keymaps({buffer = bufnr})
+    lsp.default_keymaps({ buffer = bufnr })
+
+    -- Format file on save
+    lsp.buffer_autoformat()
 end)
 
--- (Optional) Configure lua language server for neovim
+-- ensure automatic installed language servers
+lsp.ensure_installed({
+    'lua_ls',
+})
+
+-- sign icons
+lsp.set_sign_icons({
+    error = '✘',
+    warn = '▲',
+    hint = '⚑',
+    info = '»'
+})
+
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
 lsp.setup()
+
+-- Autocompletion
+-- 确保在 lsp-zero 之后设置 cmp ，这样你就可以正确地覆盖每个选项
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+local lspkind = require('lspkind')
+
+cmp.setup({
+    -- make the first item in completion menu always be selected
+    preselect = 'item',
+    completion = {
+        completeopt = 'menu,menuone,noinsert'
+    },
+    mapping = {
+        ['<Tab>'] = cmp_action.luasnip_supertab(),
+        ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
+    },
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
+    formatting = {
+        fields = { 'abbr', 'kind', 'menu' },
+        format = lspkind.cmp_format({
+            mode = 'symbol',       -- show only symbol annotations
+            maxwidth = 50,         -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+            ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+
+            -- The function below will be called before any actual modifications from lspkind
+            -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+            before = function(entry, vim_item)
+                return vim_item
+            end
+        })
+    }
+})
--- lua/roy/plugin/init.lua
+++ lua/roy/plugin/init.lua
@@ -9,7 +9,7 @@ local M = {
     },
     {
         "folke/tokyonight.nvim",
-        lazy = false, -- make sure we load this during startup if it is your main colorscheme
+        lazy = false,    -- make sure we load this during startup if it is your main colorscheme
         priority = 1000, -- make sure to load this before all the other start plugins
         config = function()
             -- load the colorscheme here
@@ -25,25 +25,28 @@ local M = {
         branch = 'v2.x',
         dependencies = {
             -- LSP Support
-            {'neovim/nvim-lspconfig'},             -- Required
-            {                                      -- Optional
-            'williamboman/mason.nvim',
-            build = function()
-                pcall(vim.cmd, 'MasonUpdate')
-            end,
-        },
-        {'williamboman/mason-lspconfig.nvim'}, -- Optional
+            { 'neovim/nvim-lspconfig' }, -- Required
+            {
+                -- Optional
+                'williamboman/mason.nvim',
+                build = function()
+                    pcall(vim.cmd, 'MasonUpdate')
+                end,
+            },
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-        -- Autocompletion
-        {'hrsh7th/nvim-cmp'},     -- Required
-        {'hrsh7th/cmp-nvim-lsp'}, -- Required
-        {'L3MON4D3/LuaSnip'},     -- Required
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },     -- Required
+            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+            { 'L3MON4D3/LuaSnip' },     -- Required
+        }
+    },
+    {
+        "onsails/lspkind.nvim"
     }
-}
 }
 
 -- vim.tbl_deep_extend
 -- local result = vim.tbl_deep_extend("force", person, someone)
 
 return M
-
