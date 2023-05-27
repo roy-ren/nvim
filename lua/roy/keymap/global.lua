bf032f670af66db094affe4c815bc0480c1b4667
roy <royite.ren@gmail.com>
2023-05-28 07:25:18 +0800

    feat: modify harpoon config

--- lua/roy/keymap/global.lua
+++ lua/roy/keymap/global.lua
@@ -3,60 +3,28 @@ local M = {}
 M.disable = {}
 
 local builtin = require('telescope.builtin')
+local mark = require("harpoon.mark")
+local ui = require("harpoon.ui")
 
 M.enable = {
     n = {
-        ["<C-s>"] = {
-            "<cmd>w<cr>",
-            args = { desc = "保存文件" }
-        },
-        ["<C-h>"] = {
-            "<C-w>h",
-            args = { desc = "切换至左边窗口" }
-        },
-        ["<C-l>"] = {
-            "<C-w>l",
-            args = { desc = "切换至右边窗口" }
-        },
-        ["<C-j>"] = {
-            "<C-w>j",
-            args = { desc = "切换至下边窗口" }
-        },
-        ["<C-k>"] = {
-            "<C-w>k",
-            args = { desc = "切换至上边窗口" }
-        },
-        ["<C-x>"] = {
-            "<cmd>NvimTreeToggle<cr>",
-            args = { desc = "Toggle folder tree" }
-        },
-        ["<leader>ff"] = {
-            builtin.find_files,
-            args = { desc = "Find files" }
-        },
-        ["<leader>fg"] = {
-            builtin.live_grep,
-            args = { desc = "live_grep" }
-        },
-        ["<leader>fb"] = {
-            builtin.buffers,
-            args = { desc = "find buffers" }
-        },
-        ["<leader>fh"] = {
-            builtin.help_tags,
-            args = { desc = "find help tags" }
-        },
+        ["<C-s>"] = { "<cmd>w<cr>", args = { desc = "保存文件" } },
+        ["<C-h>"] = { "<C-w>h", args = { desc = "切换至左边窗口" } },
+        ["<C-l>"] = { "<C-w>l", args = { desc = "切换至右边窗口" } },
+        ["<C-j>"] = { "<C-w>j", args = { desc = "切换至下边窗口" } },
+        ["<C-k>"] = { "<C-w>k", args = { desc = "切换至上边窗口" } },
+        ["<C-x>"] = { "<cmd>NvimTreeToggle<cr>", args = { desc = "Toggle folder tree" } },
+        ["<leader>ff"] = { builtin.find_files, args = { desc = "Find files" } },
+        ["<leader>fg"] = { builtin.live_grep, args = { desc = "live_grep" } },
+        ["<leader>fb"] = { builtin.buffers, args = { desc = "find buffers" } },
+        ["<leader>fh"] = { builtin.help_tags, args = { desc = "find help tags" } },
+        ["<Leader>a"] = { mark.add_file, args = { des = "add file to harpoon" } },
+        ["<C-e>"] = { ui.toggle_quick_menu, args = { des = "toggle harpoon quick menu" } },
     },
 
     i = {
-        ["<C-x>"] = {
-            "<cmd>NvimTreeToggle<cr>",
-            args = { desc = "Toggle folder tree" }
-        },
-        ["<C-s>"] = {
-            "<Esc><cmd>w<cr>",
-            args = { desc = "保存文件" }
-        }
+        ["<C-x>"] = { "<cmd>NvimTreeToggle<cr>", args = { desc = "Toggle folder tree" } },
+        ["<C-s>"] = { "<Esc><cmd>w<cr>", args = { desc = "保存文件" } }
     }
 }
 
