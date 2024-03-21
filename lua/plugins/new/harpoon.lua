5c7372119229bbba8986323cfd361c0d9c14adff
roy <royite.ren@gmail.com>
2024-03-22 02:02:15 +0800

    feat: add mappings

--- lua/plugins/new/harpoon.lua
+++ lua/plugins/new/harpoon.lua
@@ -4,7 +4,7 @@ local M = {
     require("harpoon").setup {
       menu = {
         -- width = vim.api.nvim_win_get_width(0) / 2,
-        width = 160
+        width = 120,
       },
     }
   end,
--- lua/plugins/new/noice.lua
+++ lua/plugins/new/noice.lua
@@ -16,11 +16,11 @@ local noice = {
         enabled = false,
       },
     },
-    -- you can enable a preset for easier configuration
+    -- -- you can enable a preset for easier configuration
     presets = {
-      bottom_search = true, -- use a classic bottom cmdline for search
-      command_palette = true, -- position the cmdline and popupmenu together
-      long_message_to_split = true, -- long messages will be sent to a split
+      bottom_search = false, -- use a classic bottom cmdline for search
+      command_palette = false, -- position the cmdline and popupmenu together
+      long_message_to_split = false, -- long messages will be sent to a split
       inc_rename = false, -- enables an input dialog for inc-rename.nvim
       lsp_doc_border = false, -- add a border to hover docs and signature help
     },
@@ -30,7 +30,6 @@ local noice = {
     "MunifTanjim/nui.nvim",
     -- OPTIONAL:
     --   `nvim-notify` is only needed, if you want to use the notification view.
-
     "rcarriga/nvim-notify",
   },
   config = function(_, opts)
